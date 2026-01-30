87bc28a592ad9f11d75184377aa4bac4ecf037a0
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-01-30 22:11:49 +0500

    adding more examples

---
+++ practice1/python_basics/strings1.py
@@ -0,0 +1,17 @@
+str1 = "abdurakhman"
+
+#slicing string
+print(str1[1:4])   #bdu
+
+#modify sring for example replace(), upper(), lower(), strip() - удалить пробелы справа и слева
+# split("что нибудь") -  разделить текст на несколько кусков после каждого раза встреча "..."
+
+#for ex:
+print(str1.split("a"))  #['', 'bdur', 'khm', 'n']
+
+#concatenate
+a = "hello"
+b = "world"
+
+print(a + b)
+print(a + " , " + b)
