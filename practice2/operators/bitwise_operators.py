7f12ce180bfeb81abf4eb3b66e6f3fed75566857
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-01-31 21:43:31 +0500

    adding operators

---
+++ practice2/operators/bitwise_operators.py
@@ -0,0 +1,5 @@
+# '&' - and, '|' - or, '^' - xor, '~' - not, >> - right shift, << - left shift
+a = 5  #101
+b = 15   #1111
+a &= b
+print(a)
---
+++ practice2/operators/identity_operators.py
@@ -0,0 +1,6 @@
+a = "Hello"
+b = "world"
+c = "Hello"
+print(a is b)
+print(a is c)
+print(a is not b)
