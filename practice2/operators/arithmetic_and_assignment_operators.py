c00d9df03e589434db288d4e58bd33ade9a599bc
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-01-31 21:36:00 +0500

    adding exercise

---
+++ practice2/operators/arithmetic_and_assignment_operators.py
@@ -0,0 +1,17 @@
+# +, -, /, *, **, %, //
+print(5 + 5)
+print(5 - 5)
+print(10/3)
+print(9 % 2)
+print(2 ** 4)
+print(10 // 3)
+print(5 * 5)
+
+# assignment
+a = 5
+a += 1 # a = 6
+print(a)
+a <<= 2
+print(a)
+a >>= 3
+print(a)
