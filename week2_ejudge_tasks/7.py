31535f4284e22e5eac881d5ebe037add27743a2d
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-02-14 21:24:20 +0500

    adding ejudge task solutions

--- week2_ejudge_tasks/7.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-maxx = a[0]
-position = 0
-for i in range(n):
-    if maxx < a[i]:
-        maxx = a[i]
-        position = i
-print(position + 1)
