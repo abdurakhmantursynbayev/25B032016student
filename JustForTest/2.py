c088e4db9b856caa28d0b332298347c007439d21
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-03-27 23:15:11 +0500

    adding practice7

--- JustForTest/2.py
+++ JustForTest/2.py
@@ -1,3 +1,9 @@
-a = "abdu abdu"
-if " " in a:
-    a.remove(" ")
+try:
+    def functionn(s):
+        print("hello")
+except Exception as error:
+    print(error)
+finally:
+    pass
+
+functionn(10)
